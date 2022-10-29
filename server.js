dde2caad9734a54eee537a43ed7b7c7b2cf5859f
aldotele <aldo.telese@hotmail.it>
2022-10-29 12:14:52 +0200

    added comments

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Init http and socket.io
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http);  // the library socket.io simplifies the creation of a socket
 
 // http port to listen
 const port = 3000;
@@ -13,15 +13,19 @@ const port = 3000;
 let sockets = {}
 
 // Socket.io
+// what happens when a new clients connects to a socker
 io.on('connection', function(socket) {
     console.log('Socket Connected ', socket.id);
     
+    // here the positionToServer works as a label to be sent from the client
     socket.on('positionToServer', function (data) {
         console.log('positionToServer ', data);
         
     });
 
     //Whenever someone disconnects this piece of code executed
+    // this might not require the client to send the label disconnect as it is a default action
+    // the client just disconnects and the default label is disconnect
     socket.on('disconnect', function () {
         console.log('Socket disconnected ', socket.id);
         
